46fc394738d97df87282841b5a454c235c4b2616
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2026-09-02 11:15:06 +0530

    palindrome

---
+++ 52_Day/main (1).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    while(true){
+        string word;
+        std::cout << "Enter the word to check either palindrome or not? : ";
+        std::cin >> word;
+    
+    // for (int i = 0; i < word.size(); i++) {
+    //     std::cout << word[i] << std::endl;
+    // }
+    
+    string word2;
+    for (int i = word.size()-1; i >= 0; i--) {
+        // std::cout << word[i] << std::endl;
+        word2 += word[i];
+    }
+    
+    // std::cout << word2 << std::endl;
+    
+    if(word==word2){
+        std::cout << word << " is a palindrome word." << std::endl;
+    }else{
+        std::cout << word << " is not a palindrome word." << std::endl;
+        
+    }
+    }
+
+    return 0;
+}
